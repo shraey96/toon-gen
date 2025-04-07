a705a9b306d8a9b0dfac91906d51b5e240ce6a2e
Shraey Chikker <shraeychikker@Apollo-Shraey-Chikkers-MacBook-Pro.local>
2025-04-08 01:20:48 +0530

    sitemap and robot ts files

---
+++ app/robots.ts
@@ -0,0 +1,28 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: [
+          "/",
+          "/app",
+          "/how-it-works",
+          "/features",
+          "/styles",
+          "/privacy",
+          "/terms",
+        ],
+        disallow: [
+          "/app/", // Disallow sub-routes of /app
+          "/api/",
+          "/_next/",
+          "/static/",
+        ],
+        crawlDelay: 10,
+      },
+    ],
+    sitemap: "https://zappytoon.com/sitemap.xml",
+  };
+}
